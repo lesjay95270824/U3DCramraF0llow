2f165768471923d6edbaba0dc5552e2c5e1b0080
lesjay95270824 <62696911+lesjay95270824@users.noreply.github.com>
2020-04-01 16:39:26 +0800

    相机控制脚本和人物移动控制脚本
    通过为人物相机设置旋转，上升下降，缩放和容器的父物体，改变对应的transfrom组件的变量达到相机的旋转缩放等自由视角功能。

---
+++ CamerCtl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerCtl : MonoBehaviour {
+
+    public static CamerCtl Instance;
+    /// <summary>
+    /// 控制摄像机上下
+    /// </summary>
+    [SerializeField]
+    private Transform m_CameraUPandDown;
+   
+    /// <summary>
+    /// 摄像机缩放
+    /// </summary>
+    [SerializeField]
+    private Transform m_CameraZoom;
+    /// <summary>
+    /// 摄像机容器
+    /// </summary>
+    [SerializeField]
+    private Transform m_CameraContainer;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Init()
+    {
+        m_CameraUPandDown.transform.localEulerAngles = new Vector3(0, 0, Mathf.Clamp(m_CameraUPandDown.transform.localEulerAngles.z, 35, 80));
+    }
+    /// <summary>
+    /// 摄像机旋转
+    /// </summary>
+    /// <param name="type">0=左，1=右</param>
+    public void SetCameraRotate(int type)
+    {
+        transform.Rotate(0, 20 * Time.deltaTime * (type == 0 ? -1 : 1), 0);
+    }
+    /// <summary>
+    /// 设置摄像机上下
+    /// </summary>
+    /// <param name="type">0=上，1=下</param>
+    public void SetCameraUpandDomn(int type)
+    {
+        m_CameraUPandDown.transform.Rotate(0, 0, 15 * Time.deltaTime * (type == 1 ? -1 : 1), 0);
+        //插值保证在一定范围上下
+        m_CameraUPandDown.transform.localEulerAngles = new Vector3(Mathf.Clamp(m_CameraUPandDown.transform.localEulerAngles.x, 10, 80), 0,0);
+    }
+    /// <summary>
+    /// 设置摄像机缩放
+    /// </summary>
+    /// <param name="type">0=拉远，1=拉近</param>
+    public void SetCameraZoom(int type)
+    {
+        m_CameraZoom.Translate(Vector3.forward * 10 * Time.deltaTime * (type == 1 ? -1 : 1));
+        m_CameraZoom.localPosition = new Vector3(0, 0, Mathf.Clamp(m_CameraZoom.localPosition.z,-8.5f,-6.0f)); 
+    }
+    /// <summary>
+    /// 朝向物体
+    /// </summary>
+    public void AutoLookAt(Vector3 pos)
+    {
+        m_CameraZoom.LookAt(pos); 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,13f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 11f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 10f);
+    }
+}
---
+++ RoleCtl.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleCtl : MonoBehaviour {
+
+    /// <summary>
+    /// 移动的目标点
+    /// </summary>
+	private Vector3 m_TargetPos = Vector3.zero;
+    /// <summary>
+    /// 移动的速度
+    /// </summary>
+    public float m_Speed = 10.0f;
+
+    public float m_RotationSpeed = 0.5f;
+
+    public Quaternion m_TarfeQuaternion;
+
+    public bool IsRota=false;
+    public CharacterController m_characterController;
+	void Start ()
+    {
+        m_characterController = GetComponent<CharacterController>(); 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hiyInfo;
+            if(Physics.Raycast(ray,out hiyInfo))
+            {
+                if (hiyInfo.collider.name.Equals("Plane",System.StringComparison.CurrentCultureIgnoreCase))
+                {
+                    m_TargetPos = hiyInfo.point;
+                    m_RotationSpeed = 0.0f;
+                    IsRota = true;
+                }
+            }
+        }
+        if (!m_characterController.isGrounded)
+        {
+            m_characterController.Move(transform.position + new Vector3(0, -100, 0)- transform.position); 
+        }
+            if (m_TargetPos != Vector3.zero)
+            {
+                if (Vector3.Distance(m_TargetPos, transform.position) > 0.1f)
+                {
+                    Vector3 direction = m_TargetPos - transform.position;
+                    direction = direction.normalized;
+                    direction = direction * m_Speed * Time.deltaTime;
+                    direction.y = 0;
+                // transform.LookAt(new Vector3(m_TargetPos.x,  transform.position.y, m_TargetPos.z));
+                if (IsRota)
+                {
+                    m_RotationSpeed += 5.0f;
+                    m_TarfeQuaternion = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, m_TarfeQuaternion, m_RotationSpeed * Time.deltaTime);
+
+                    if (Quaternion.Angle(m_TarfeQuaternion, transform.rotation) < 1f)
+                    {
+                        m_RotationSpeed = 1;
+                        IsRota = false;
+                    }
+                }
+                    m_characterController.Move(direction);
+                }
+            }
+        CameraAutoFollow();
+        if(Input.GetKey(KeyCode.A))
+        {
+            CamerCtl.Instance.SetCameraRotate(0);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            CamerCtl.Instance.SetCameraRotate(1);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            CamerCtl.Instance.SetCameraUpandDomn(0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            CamerCtl.Instance.SetCameraUpandDomn(1);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+           
+            CamerCtl.Instance.SetCameraZoom(0);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            CamerCtl.Instance.SetCameraZoom(1);
+        }
+    }
+
+    private void CameraAutoFollow()
+    {
+        if (CamerCtl.Instance == null)
+            return;
+        CamerCtl.Instance.transform.position = gameObject.transform.position;
+        CamerCtl.Instance.AutoLookAt(gameObject.transform.position);
+    }
+}
